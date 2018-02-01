e23241405100331ff110ecb5ee9aea13f1ab79e6
PRANAV VENKATA <pranav.venkata@gmail.com>
2018-02-01 17:35:57 +0530

    MainActivity Changed

--- app/src/main/java/com/actigage/whitelabelingdemo/MainActivity.java
+++ app/src/main/java/com/actigage/whitelabelingdemo/MainActivity.java
@@ -16,6 +16,7 @@ import fragments.RedFragment1;
 import fragments.RedFragment2;
 import fragments.RedFragment3;
 
+
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -27,44 +28,24 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-        setupViewPagerAndTab(viewPager,BuildConfig.FLAVOR);
+        setupViewPagerAndTab(viewPager);
     }
     @Override
     protected void onStart() {
         super.onStart();
     }
-    private void setupViewPagerAndTab(ViewPager viewPager,String Flavor) {
+    private void setupViewPagerAndTab(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        switch (Flavor) {
-            case "redbottomnavigation":
-                adapter.addFragment(new RedFragment1(), "RED1");
-                adapter.addFragment(new RedFragment2(), "RED2");
-                adapter.addFragment(new RedFragment3(), "RED3");
-                break;
-            case "greenbottomnavigation":
-                break;
-            case "yellowbottomnavigation":
-                break;
-            default:
-                break;
-        }
+        adapter.addFragment(new RedFragment1(), "RED1");
+        adapter.addFragment(new RedFragment2(), "RED2");
+        adapter.addFragment(new RedFragment3(), "RED3");
         viewPager.setAdapter(adapter);
-        setupTabIcons(Flavor);
+        setupTabIcons();
     }
-    private void setupTabIcons(String Flavor) {
-        switch (Flavor) {
-            case "redbottomnavigation":
-                tabLayout.getTabAt(0).setIcon(R.drawable.agenda);
-                tabLayout.getTabAt(1).setIcon(R.drawable.sponsors);
-                tabLayout.getTabAt(2).setIcon(R.drawable.speakers);
-                break;
-            case "greenbottomnavigation":
-                break;
-            case "yellowbottomnavigation":
-                break;
-            default:
-                break;
-        }
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(R.drawable.agenda);
+        tabLayout.getTabAt(1).setIcon(R.drawable.sponsors);
+        tabLayout.getTabAt(2).setIcon(R.drawable.speakers);
     }
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
