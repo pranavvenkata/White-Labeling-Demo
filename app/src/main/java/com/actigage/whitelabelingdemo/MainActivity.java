c371e66299733db135fb0d0e99053265b9c100a3
PRANAV VENKATA <pranav.venkata@gmail.com>
2018-02-01 22:10:13 +0530

    Changes in Yellow Flavor

--- app/src/main/java/com/actigage/whitelabelingdemo/MainActivity.java
+++ app/src/main/java/com/actigage/whitelabelingdemo/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.List;
 
+import fragments.YellowFragment1;
+import fragments.YellowFragment2;
+
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -20,19 +23,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-
+        setupViewPager(viewPager);
+        setupTabIcons();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
     }
+
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-
+        adapter.addFragment(new YellowFragment1(), "YELLOW1");
+        adapter.addFragment(new YellowFragment2(), "YELLOW2");
         viewPager.setAdapter(adapter);
+    }
+    private void setupTabIcons(){
+        tabLayout.getTabAt(0).setIcon(R.drawable.agenda);
+        tabLayout.getTabAt(1).setIcon(R.drawable.speakers);
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
