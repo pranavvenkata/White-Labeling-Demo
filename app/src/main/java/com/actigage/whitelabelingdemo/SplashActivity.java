92d379ce3467ba58a57a9329182f2a66614369bd
PRANAV VENKATA <pranav.venkata@gmail.com>
2018-02-02 11:19:33 +0530

    Added Splash Screen and launcher icon to Red Flavor

---
+++ app/src/main/java/com/actigage/whitelabelingdemo/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.actigage.whitelabelingdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash2);
+        new Handler().postDelayed(new Runnable() {
+             @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
